9b9ce4f4ae05ff36174e951ddf831b5c732f018e
Auggie <miaugus@gmail.com>
2022-07-24 20:45:13 -0400

    questions working

--- index.js
+++ index.js
@@ -7,94 +7,119 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-const Employees = [];
+const employeeArray = [];
+
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter team manager's name",
+        name: "managerName",
+      },
+      {
+        type: "input",
+        message: "Enter manager's ID",
+        name: "managerId",
+      },
+      {
+        type: "input",
+        message: "Enter manager's email address",
+        name: "managerEmail",
+      },
+      {
+        type: "input",
+        message: "Enter employee's office number",
+        name: "managerOffice",
+      },
+    ])
+    .then((managerAnswers) => {
+      const newManager = new Manager(
+        managerAnswers.managerName,
+        managerAnswers.managerId,
+        managerAnswers.managerEmail,
+        managerAnswers.managerOffice
+      );
+      employees.push(newManager);
+      createTeam();
+    });
+}
+
+function createTeam() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to add any additional members to the team?",
+        name: "addEmployee",
+        choices: [
+          "Add an Engineer",
+          "Add an Intern",
+          "No additional employees",
+        ],
+      },
+      {
+        when: (complete) => complete.addEmployee !== "No additional employees",
+        type: "input",
+        message: "Enter employee's name",
+        name: "employeeName",
+      },
+      {
+        when: (complete) => complete.addEmployee !== "No additional employees",
+        type: "input",
+        message: "Enter employee's ID",
+        name: "employeeId",
+      },
+      {
+        when: (complete) => complete.addEmployee !== "No additional employees",
+        type: "input",
+        message: "Enter employee's email",
+        name: "employeeEmail",
+      },
+      {
+        when: (complete) => complete.addEmployee === "Add an Engineer",
+        type: "input",
+        message: "Enter Engineer's GitHub username",
+        name: "engineerGitHub",
+      },
+      {
+        when: (complete) => complete.addEmployee === "Add an Intern",
+        type: "input",
+        message: "Enter name of Intern's school",
+        name: "internSchool",
+      },
+    ])
+    .then((employeeProfile) => {
+      if (employeeProfile.addEmployee === "Add an Engineer") {
+        const engineer = new Engineer(
+          employeeProfile.employeeName,
+          employeeProfile.employeId,
+          employeeProfile.employeeEmail,
+          employeeProfile.engineerGitHub
+        );
+        employeeArray.push(engineer);
+        createTeam();
+      } else if (employeeProfile.addEmployee === "Add an Intern") {
+        const intern = new Intern(
+          employeeProfile.employeeName,
+          employeeProfile.employeId,
+          employeeProfile.employeeEmail,
+          employeeProfile.internSchool
+        );
+        employeeArray.push(intern);
+        createTeam();
+      } else if (employeeProfile.addEmployee === "No additional employees") {
+        createHTML();
+      }
+    });
+}
+
+function writeFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 function begin() {
-  function addEmployee() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "Enter team manager's name",
-          name: "managerName",
-        },
-        {
-          type: "input",
-          message: "Enter manager's ID",
-          name: "managerId",
-        },
-        {
-          type: "input",
-          message: "Enter manager's email address",
-          name: "managerEmail",
-        },
-        {
-          type: "input",
-          message: "Enter employee's office number",
-          name: "managerOffice",
-        },
-      ])
-      .then((answers) => {
-        const newManager = new Manager(
-          answers.managerName,
-          answers.managerId,
-          answers.managerEmail,
-          managerOffice
-        );
-        Employees.push(newManager);
-        createTeam();
-      });
-  }
-
-  function createTeam() {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          message: "Would you like to add any additional members to the team?",
-          name: "addEmployee",
-          choices: [
-            "Add an Engineer",
-            "Add an Intern",
-            "No additional employees",
-          ],
-        },
-        {
-          type: "input",
-          message: "Enter employee's name",
-          name: "employeeName",
-        },
-        {
-          type: "input",
-          message: "Enter employee's ID",
-          name: "employeeId",
-        },
-        {
-          type: "input",
-          message: "Enter employee's email",
-          name: "employeeEmail",
-        },
-      ])
-      .then(function ({
-        addEmployee,
-        employeeName,
-        employeeId,
-        employeeEmail,
-      }) {
-        let employeeInfo = "";
-        if (addEmployee === "Engineer") {
-          employeeInfo = "GitHub username";
-        } else if (addEmployee === "Intern") {
-          employeeInfo = "school name";
-        }
-        inquirer.prompt([
-          {
-            type: "input",
-            message: `Enter ${employeeInfo} for employee`,
-          },
-        ]);
-      });
-  }
+  createTeam();
 }
 
 begin();
